0e6e581f93abbf09fa46e77848e439a6f259b17e
Schimmoellerm <schimmoellerm@gmail.com>
2021-02-09 21:43:41 -0500

    Started Java Script, have basics need if statements. changed Html to be on Click.

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,37 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
+
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "0123456789";
+let specialCharacters =  " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+let charPool = ""
+
+function generateBtn () {
+  let input = prompt("Select a password length between 8 and 128");
+
+  //Need if statement that will check if the 
+  //if (input < 8);
+   // alert ("password length is too short");
+   // generateBtn;
+//} else if (input > 128); {
+    //alert ("password is to long");
+    //generateBtn;
+//}
+
+  let choiceLower = confirm("Do you want to include lowercase letters?");
+//Need if statement for including lowers
+  let choiceUpper = confirm("Do you want to include uppercase letters?");
+//Need if statement for including uppers
+  let choiceNumber = confirm("Do you want to include numbers?");
+//Need if statement for including numbers
+  let choiceSpecial = confirm("Do you want to include special characters?");
+//Need if statement for including specials
+
+//.fromCharCode to pull key binding information from UTF-8
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +43,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
