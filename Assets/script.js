02ceeae01d5bcbed13010760044a25ae2432662c
Schimmoellerm <schimmoellerm@gmail.com>
2021-02-10 17:57:57 -0500

    added a reset for charPool

--- Assets/script.js
+++ Assets/script.js
@@ -13,28 +13,34 @@ function generateBtn () {
     alert ("Unacceptable length! Please start over.");
     generateBtn();
   } else {
+    console.log(charPool);
     let choiceLower = confirm("Do you want to include lowercase letters?");
       if (choiceLower === true) {
         charPool = charPool + lowercase;
+        console.log(charPool);
       }
     let choiceUpper = confirm("Do you want to include uppercase letters?");
       if (choiceUpper === true) {
         charPool = charPool + uppercase;
+        console.log(charPool);
       }
     let choiceNumber = confirm("Do you want to include numbers?");
       if (choiceNumber === true) {
         charPool = charPool + numbers;
+        console.log(charPool);
       }
     let choiceSpecial = confirm("Do you want to include special characters?");
       if (choiceSpecial === true) {
         charPool = charPool + specialCharacters;
+        console.log(charPool);
       }
     if (!choiceLower && !choiceUpper && !choiceNumber && !choiceSpecial) {
       alert ("You must make at least one selection! Please start over.");
       generateBtn();
     }
   }
-
+  charPool = "";
+  console.log(charPool);
 //.fromCharCode to pull key binding information from UTF-8
 }
 
