66b4e7102055f39cacddec1d83185130c1a8ec06
Schimmoellerm <schimmoellerm@gmail.com>
2021-02-11 17:03:37 -0500

    Completed functionality and check for selected elements. Need to add comments and complete Readme.

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,14 @@
 // Assignment Code
 
 let password = document.querySelector("#password")
-let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numbers = "0123456789";
 let specialCharacters =  " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 let charPool = "";
 let finalPassword = "";
 let input;
+let counter = 0;
 
 function generateBtn () {
   let input = prompt("Select a password length between 8 and 128");
@@ -20,28 +21,41 @@ function generateBtn () {
       if (choiceLower === true) {
         charPool = charPool + lowercase;
         console.log(charPool);
+        counter++;
+        finalPassword = finalPassword + lowercase[Math.floor(Math.random() * lowercase.length)];
+        console.log(finalPassword);
       }
     let choiceUpper = confirm("Do you want to include uppercase letters?");
       if (choiceUpper === true) {
         charPool = charPool + uppercase;
         console.log(charPool);
+        counter++;
+        finalPassword = finalPassword + uppercase[Math.floor(Math.random() * uppercase.length)];
+        console.log(finalPassword);
       }
     let choiceNumber = confirm("Do you want to include numbers?");
       if (choiceNumber === true) {
         charPool = charPool + numbers;
         console.log(charPool);
+        counter++;
+        finalPassword = finalPassword + numbers[Math.floor(Math.random() * numbers.length)];
+        console.log(finalPassword);
       }
     let choiceSpecial = confirm("Do you want to include special characters?");
       if (choiceSpecial === true) {
         charPool = charPool + specialCharacters;
         console.log(charPool);
+        counter++;
+        finalPassword = finalPassword + specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+        console.log(finalPassword);
       }
     if (!choiceLower && !choiceUpper && !choiceNumber && !choiceSpecial) {
       alert ("You must make at least one selection! Please start over.");
+      counter = 0;
       generateBtn();
     }
   }
-  for (i = 0; i < input; i++) {
+  for (i = 0; i < input - counter; i++) {
     let Choices = charPool[Math.floor(Math.random() * charPool.length)];
     finalPassword = finalPassword + Choices;
   }
@@ -49,6 +63,7 @@ function generateBtn () {
   password.textContent = finalPassword;
   charPool = "";
   finalPassword = "";
+  counter = 0;
   console.log(charPool);
   console.log(finalPassword);
 
